f2de5c9ea025631cfde2208e04f426253fea14d0
Brad <bycarlson23@gmail.com>
2022-11-07 12:10:37 -0600

    added contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,20 +1,46 @@
 import React, { useState } from 'react';
 
-export default function Contact() {
 
-    const [name, putName] = useState('');
-    const [email, putEmail] = useState('');
-    const [message, putMessage] = useState('');
-
-
-
-
+const ContactForm = () => {
+    const [formStatus, setFormStatus] = useState('Send')
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setFormStatus('Submitting...')
+        const { name, email, message } = e.target.elements
+        let conFom = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        }
+        console.log(conFom)
+    }
     return (
-        <div>
-            <h1>Contact Me!</h1>
-            <p>
-
-            </p>
+        <div className="container mt-5">
+            <h2 className="mb-3">Contact me!</h2>
+            <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="name">
+                        Name
+                    </label>
+                    <input className="form-control" type="text" id="name" required />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="email">
+                        Email
+                    </label>
+                    <input className="form-control" type="email" id="email" required />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="message">
+                        Message
+                    </label>
+                    <textarea className="form-control" id="message" required />
+                </div>
+                <button className="btn btn-danger" type="submit">
+                    {formStatus}
+                </button>
+            </form>
         </div>
-    );
+    )
 }
+export default ContactForm
