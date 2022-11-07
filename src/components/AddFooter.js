70717d374c15fe2125191dca66daae42f5f97c79
Brad <bycarlson23@gmail.com>
2022-11-07 14:49:11 -0600

    functioning footer w correct links

--- src/components/AddFooter.js
+++ src/components/AddFooter.js
@@ -1,9 +1,26 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
+
+const styles = {
+    footer: {
+        position: 'fixed'
+    }
+}
 
 const AddFooter = () => {
     return (<>
-        <footer className="bg-success text-white text-center m-3 -3 w-100">
-            <h6>&copy; Brad Carlson</h6>
+        <footer style={styles.footer} className="bg-success text-white text-center m-3 -3 w-100">
+            {/* <h6>&copy; Brad Carlson</h6> */}
+
+            <SocialIcon url="https://github.com/brad1200" />
+            <a href="https://github.com/brad1200"></a>
+
+            <SocialIcon url="https://www.linkedin.com/in/brad-carlson-04153623b/" />
+            <a href="https://www.linkedin.com/in/brad-carlson-04153623b/"></a>
+
+            <SocialIcon url="https://www.instagram.com/_brad_23" />
+            <a href="https://www.instagram.com/_brad_23"></a>
         </footer>
     </>)
 }
